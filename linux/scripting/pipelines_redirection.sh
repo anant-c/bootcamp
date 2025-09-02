d64d64a812d114d4cacc07fe9d5bad2543d8b826
anant-c <canant448@gmail.com>
2025-09-02 10:24:24 +0530

    pipelines and redirections

--- linux/scripting/pipelines_redirection.sh
+++ linux/scripting/pipelines_redirection.sh
@@ -1,22 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# List all files in /usr/bin that contain the word "python" in their names.
-ls -l /usr/bin | grep "python"
+set -e
 
-# Count the number of files in /usr/bin.
-ls -1 /usr/bin | wc -l
+step(){
+  echo -e "[Step $1] $2 \n"
+}
 
-# Save the list of files in your home directory to a file named myfiles.txt.
-ls -1 /usr/bin > ~/myfiles.txt
+safe_exit(){
+  echo -e "[ERROR]: $1 \n"
+  exit 1
+}
 
-# Append the output of date to myfiles.txt.
-DATE=$(date)
-echo $DATE >> ~/myfiles.txt
-# or can do=> date >> ~/myfiles.txt
+step 1 "List all files in /usr/bin that contain the word "python" in their names."
+ls -l /usr/bin | grep "python" || safe_exit "Failed to get all files"  
+sleep 2
 
+step 2 "Count the number of files in /usr/bin."
+ls -1 /usr/bin | wc -l || safe_exit "Failed to count the number of file in usr/bin"
+sleep 2
+step 3 "Save the list of files in your home directory to a file named myfiles.txt."
+ls -1 /usr/bin > ~/myfiles.txt || safe_exit "Failed to save in myfiles.txt"
+sleep 2
+step 4 "Append the output of date to myfiles.txt."
+
+date >> ~/myfiles.txt || safe_exit "Failed to append date in myfiles.txt"
+sleep 2
 # check using tail 
 tail ~/myfiles.txt
+sleep 2
 
-# Sort the contents of myfiles.txt alphabetically and save the result in sorted_myfiles.txt
-sort ~/myfiles.txt > ~/sorted_myfiles.txt
+step 5 "Sort the contents of myfiles.txt alphabetically and save the result in sorted_myfiles.txt"
+sort ~/myfiles.txt > ~/sorted_myfiles.txt || safe_exit "Failed to sort and store."
 
